7b2706e4e5c9cbd36362cd789dc5e38fa4a3d87a
Kalda <84191272+just-kalda@users.noreply.github.com>
2025-01-16 04:01:20 +0300

    Create module_8_1.py

---
+++ module_8_1.py
@@ -0,0 +1,43 @@
+class Product():
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+    
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+        
+
+class Shop():
+    def __init__(self):
+        self.__file_name = "products.txt"
+    
+    def get_products(self):
+        self.file = open(self.__file_name, 'r')
+        self.readed = self.file.read()
+        self.file.close()
+        return self.readed
+    
+    def add(self, *products: Product):
+        names = []
+        file = open(self.__file_name, 'a+')
+        file.seek(0)
+        for line in file:
+            name = line.split(', ')[0]
+            names.append(name)
+        for product in products:
+            if product.name in names:
+                print(f'Продукт {product.name} уже есть в магазине')
+            else:
+                file.write(f'{product}\n')
+                names.append(product.name)
+        file.close()
+        
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+print(p2)
+s1.add(p1, p2, p3)
+print(s1.get_products())
